3c76860173acadd69ef122f58683ef1297f4e9d8
Ewan Cahen <e.cahen@esciencecenter.nl>
2026-04-20 14:42:01 +0200

    chore: use new software list

--- src/main/java/nl/esciencecenter/EstroSoftware.java
+++ src/main/java/nl/esciencecenter/EstroSoftware.java
@@ -18,31 +18,35 @@ public record EstroSoftware(
 	private static final Pattern DOI_PATTERN = Pattern.compile("^10(\\.\\w+)+/\\S+$");
 
 	static EstroSoftware fromCsvLine(String line) {
-		String[] split = line.split("\\|");
-		String name = split[0];
-		String shortStatement = split[12];
+		try {
+			String[] split = line.split("\\|");
+			String name = split[0];
+			String shortStatement = split[12];
 
-		String keyword = split[3];
+			String keyword = split[3];
 
-		String rawEstroField = split[4];
-		Optional<String> estroField = rawEstroField == null || rawEstroField.isBlank() ? Optional.empty() : Optional.of(rawEstroField);
+			String rawEstroField = split[4];
+			Optional<String> estroField = rawEstroField == null || rawEstroField.isBlank() ? Optional.empty() : Optional.of(rawEstroField);
 
-		Optional<URI> website = extractUrl(split[8]);
+			Optional<URI> website = extractUrl(split[8]);
 
-		Optional<URI> gitUrl = extractUrl(split[10]);
+			Optional<URI> gitUrl = extractUrl(split[10]);
 
-		String rawDoi = split[13];
-		Optional<String> doi = DOI_PATTERN.matcher(rawDoi).find() ? Optional.of(rawDoi) : Optional.empty();
+			String rawDoi = split[13];
+			Optional<String> doi = DOI_PATTERN.matcher(rawDoi).find() ? Optional.of(rawDoi) : Optional.empty();
 
-		return new EstroSoftware(
-				name,
-				shortStatement,
-				keyword,
-				estroField,
-				website,
-				gitUrl,
-				doi
-		);
+			return new EstroSoftware(
+					name,
+					shortStatement,
+					keyword,
+					estroField,
+					website,
+					gitUrl,
+					doi
+			);
+		} catch (RuntimeException e) {
+			throw new RuntimeException(line, e);
+		}
 	}
 
 	private static Optional<URI> extractUrl(String rawUrl) {
